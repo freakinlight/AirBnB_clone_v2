4bdc7a7526caa286a180e1ed23dbecd475c839c8
freakinlight <mazouznour0@gmail.com>
2024-07-28 18:30:24 +0000

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,8 +21,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ Displays 'C ' followed by the value of the text variable, 
-    replacing underscores with spaces. """
+    """ Displays 'C ' followed by the value of the text variable """
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == "__main__":
