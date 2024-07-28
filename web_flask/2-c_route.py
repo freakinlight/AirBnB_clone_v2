33f9f7a51cbd28ee9d2779c35f9f040ece9d9ceb
freakinlight <mazouznour0@gmail.com>
2024-07-28 18:23:50 +0000

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A Flask web application that listens on 0.0.0.0, port 5000.
+The application includes multiple routes displaying various messages.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Displays 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Displays 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Displays 'C ' followed by the value of the text variable, 
+    replacing underscores with spaces. """
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
